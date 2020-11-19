46f9231b106275c818686583c8e9dce4bdba8627
Alexandre Guillaume Nicolas CAMBEFORT <acambefort@pentalog.com>
2020-11-19 13:38:31 +0200

    product now display correctly on frontpage

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'shopapp'
+
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('category_slug/', views.product_list, name='product_list_by_category'),
-    path('slug/', views.product_detail, name='product_detail'),
+    path('<category_slug>/', views.product_list, name='product_list_by_category'),
+    path('<id>/<slug>/', views.product_detail, name='product_detail'),
 ]
--- shopapp/views.py
+++ shopapp/views.py
@@ -5,11 +5,21 @@ def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    print('category_slug :', category_slug)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
+    print('categories: ', categories)
+    print('product: ', products)
+    print('category: ', category)
     return render(request,
-                  'shop/product/list.html',
-                    {'category':category,
-                     'categories':categories,
-                     'products':products})
+                  'shopapp/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request,
+                  'shopapp/product/detail.html',
+                  {'product':product})
