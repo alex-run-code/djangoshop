6c6664fa7a5e36f234b738436a02f46085a6f469
Alexandre Guillaume Nicolas CAMBEFORT <acambefort@pentalog.com>
2020-11-23 13:06:29 +0200

    Order admin created

--- shop/settings.py
+++ shop/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shopapp',
+    'cart',
+    'order',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
@@ -123,3 +126,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+# Cart session
+CART_SESSION_ID = 'cart'
